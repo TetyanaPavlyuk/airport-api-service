e98201d91680e291133375c382dbaa644bcbb7fa
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-08-01 11:21:44 +0300

    Create models for airport app

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,152 @@
+from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    closest_big_city = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Route(models.Model):
+    source = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    distance = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.source.name} - {self.destination.name} ({self.distance} km)"
+
+    class Meta:
+        ordering = ["source", "destination"]
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE)
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return f"{self.name} ({self.airplane_type.name})"
+
+    class Meta:
+        ordering = ["name"]
+
+
+class CrewPosition(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    position = models.ForeignKey(CrewPosition, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.position}: {self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ["position", "first_name", "last_name"]
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.created_at
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    crew = models.ManyToManyField(Crew)
+
+    def __str__(self):
+        return (f"{self.route.source} ({self.departure_time}) - "
+                f"{self.route.destination} ({self.arrival_time})")
+
+    class Meta:
+        ordering = ["departure_time", "route", "airplane"]
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    @staticmethod
+    def validate_ticket(row, seat, airplane, error_to_raise):
+        for ticket_attr_name, ticket_attr_value, airplane_attr_name in [
+            ("row", row, "rows"),
+            ("seat", seat, "seats_in_row")
+        ]:
+            count_attrs = getattr(airplane, airplane_attr_name)
+            if not (0 < ticket_attr_value < count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} number must"
+                                          f"be in available range: "
+                                          f"(1, {airplane_attr_name}): "
+                                          f"(1, {count_attrs}"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.flight.airplane,
+            ValidationError
+        )
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    def __str__(self):
+        return f"{self.flight} (seat: {self.seat}, row: {self.row})"
+
+    class Meta:
+        ordering = ["-order", "flight"]
+        unique_together = ["row", "seat", "flight"]

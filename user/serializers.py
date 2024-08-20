70d573c76c455685801aab2016059830aa4bdbd2
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-08-20 13:30:21 +0300

    Add style for input password (hide the password when entering) in UserSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -14,7 +14,11 @@ class UserSerializer(serializers.ModelSerializer):
         )
         read_only_fields = ("id", "is_staff")
         extra_kwargs = {
-            "password": {"write_only": True, "min_length": 8}
+            "password": {
+                "write_only": True,
+                "min_length": 8,
+                "style": {"input_type": "password"}
+            }
         }
 
     def create(self, validated_data):

99113c268b40b38ccc5605921239028409ffdfc9
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-08-27 11:53:09 +0300

    Create tests for filtration by flights

--- airport/tests/test_flight_api.py
+++ airport/tests/test_flight_api.py
@@ -111,15 +111,15 @@ class AuthenticatedFlightAPITests(TestCase):
         self.client.force_authenticate(user=self.user)
 
     def test_flight_list(self):
-        flight1 = sample_flight()
-        flight2 = sample_flight()
+        flight = sample_flight()
+        sample_flight()
         order = Order.objects.create(
             user=self.user,
         )
-        ticket = Ticket.objects.create(
+        Ticket.objects.create(
             row=5,
             seat=3,
-            flight=flight1,
+            flight=flight,
             order=order
         )
 
@@ -131,3 +131,242 @@ class AuthenticatedFlightAPITests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["results"], serializer.data)
 
+    def test_filter_flights_by_source_airport(self):
+        airport_in = Airport.objects.create(name="Airport1")
+        airport_out = Airport.objects.create(name="Airport2")
+        route_in = Route.objects.create(
+            source=airport_in,
+            destination=airport_out,
+            distance=600
+        )
+        route_out = Route.objects.create(
+            source=airport_out,
+            destination=airport_in,
+            distance=600
+        )
+        flight_in = sample_flight(route=route_in)
+        flight_out = sample_flight(route=route_out)
+
+        response = self.client.get(
+            FLIGHT_URL,
+            {"source_airport": f"{airport_in.name}"}
+        )
+
+        serializer_in = FlightListSerializer(flight_in)
+        serializer_out = FlightListSerializer(flight_out)
+        self.assertIn(serializer_in.data, response.data["results"])
+        self.assertNotIn(serializer_out.data, response.data["results"])
+
+    def test_filter_flights_by_destination_airport(self):
+        airport_in = Airport.objects.create(name="Airport1")
+        airport_out = Airport.objects.create(name="Airport2")
+        route_in = Route.objects.create(
+            source=airport_out,
+            destination=airport_in,
+            distance=600
+        )
+        route_out = Route.objects.create(
+            source=airport_in,
+            destination=airport_out,
+            distance=600
+        )
+        flight_in = sample_flight(route=route_in)
+        flight_out = sample_flight(route=route_out)
+
+        response = self.client.get(
+            FLIGHT_URL,
+            {"destination_airport": f"{airport_in.name}"}
+        )
+
+        serializer_in = FlightListSerializer(flight_in)
+        serializer_out = FlightListSerializer(flight_out)
+
+        self.assertIn(serializer_in.data, response.data["results"])
+        self.assertNotIn(serializer_out.data, response.data["results"])
+
+    def test_filter_flights_by_source_city(self):
+        airport_in = Airport.objects.create(
+            name="Airport1",
+            closest_big_city="City1"
+        )
+        airport_out = Airport.objects.create(
+            name="Airport2",
+            closest_big_city="City2"
+        )
+        route_in = Route.objects.create(
+            source=airport_in,
+            destination=airport_out,
+            distance=600
+        )
+        route_out = Route.objects.create(
+            source=airport_out,
+            destination=airport_in,
+            distance=600
+        )
+        flight_in = sample_flight(route=route_in)
+        flight_out = sample_flight(route=route_out)
+
+        response = self.client.get(
+            FLIGHT_URL,
+            {"source_city": f"{airport_in.closest_big_city}"}
+        )
+
+        serializer_in = FlightListSerializer(flight_in)
+        serializer_out = FlightListSerializer(flight_out)
+        self.assertIn(serializer_in.data, response.data["results"])
+        self.assertNotIn(serializer_out.data, response.data["results"])
+
+    def test_filter_flights_by_destination_city(self):
+        airport_in = Airport.objects.create(
+            name="Airport1",
+            closest_big_city="City1"
+        )
+        airport_out = Airport.objects.create(
+            name="Airport2",
+            closest_big_city="City2"
+        )
+        route_in = Route.objects.create(
+            source=airport_out,
+            destination=airport_in,
+            distance=600
+        )
+        route_out = Route.objects.create(
+            source=airport_in,
+            destination=airport_out,
+            distance=600
+        )
+        flight_in = sample_flight(route=route_in)
+        flight_out = sample_flight(route=route_out)
+
+        response = self.client.get(
+            FLIGHT_URL,
+            {"destination_city": f"{airport_in.closest_big_city}"}
+        )
+
+        serializer_in = FlightListSerializer(flight_in)
+        serializer_out = FlightListSerializer(flight_out)
+
+        self.assertIn(serializer_in.data, response.data["results"])
+        self.assertNotIn(serializer_out.data, response.data["results"])
+
+    def test_filter_flights_by_airplane(self):
+        airplane_type = AirplaneType.objects.create(name="Airplane Type")
+
+        airplane_in = Airplane.objects.create(
+            name="Airplane1",
+            rows=20,
+            seats_in_row=8,
+            airplane_type=airplane_type
+        )
+        airplane_out = Airplane.objects.create(
+            name="Airplane2",
+            rows=20,
+            seats_in_row=8,
+            airplane_type=airplane_type
+        )
+
+        flight_in = sample_flight(airplane=airplane_in)
+        flight_out = sample_flight(airplane=airplane_out)
+
+        response = self.client.get(
+            FLIGHT_URL,
+            {"airplane": f"{airplane_in.name}"}
+        )
+
+        serializer_in = FlightListSerializer(flight_in)
+        serializer_out = FlightListSerializer(flight_out)
+
+        self.assertIn(serializer_in.data, response.data["results"])
+        self.assertNotIn(serializer_out.data, response.data["results"])
+
+    def test_filter_flights_by_crew(self):
+        crew_position = CrewPosition.objects.create(name="Crew Position")
+
+        crew1 = Crew.objects.create(
+            first_name="First Name 1",
+            last_name="Last Name 1",
+            position=crew_position
+        )
+        crew2 = Crew.objects.create(
+            first_name="First Name 2",
+            last_name="Last Name 2",
+            position=crew_position
+        )
+        crew3 = Crew.objects.create(
+            first_name="First Name 3",
+            last_name="Last Name 3",
+            position=crew_position
+        )
+
+        flight1_in = sample_flight()
+        flight1_in.crew.add(crew1)
+        flight2_in = sample_flight()
+        flight2_in.crew.add(crew2)
+        flight2_in.crew.add(crew3)
+        flight_out = sample_flight()
+        flight_out.crew.add(crew3)
+
+        response = self.client.get(
+            FLIGHT_URL,
+            {"crew": f"{crew1.id}, {crew2.id}"}
+        )
+
+        serializer1_in = FlightListSerializer(flight1_in)
+        serializer2_in = FlightListSerializer(flight2_in)
+        serializer_out = FlightListSerializer(flight_out)
+
+        self.assertIn(serializer1_in.data, response.data["results"])
+        self.assertIn(serializer2_in.data, response.data["results"])
+        self.assertNotIn(serializer_out.data, response.data["results"])
+
+    def test_filter_flights_by_date_departure(self):
+        flight_eq = sample_flight(
+            departure_time="2024-08-27 15:00:00+03:00",
+            arrival_time="2024-08-27 17:00:00+03:00",
+        )
+        flight_qt = sample_flight(
+            departure_time="2024-08-28 15:00:00+03:00",
+            arrival_time="2024-08-28 17:00:00+03:00",
+        )
+        flight_lt = sample_flight(
+            departure_time="2024-08-26 15:00:00+03:00",
+            arrival_time="2024-08-26 17:00:00+03:00",
+        )
+
+        response = self.client.get(
+            FLIGHT_URL, {"date_departure": "2024-08-27"}
+        )
+
+        serializer_eq = FlightListSerializer(flight_eq)
+        serializer_qt = FlightListSerializer(flight_qt)
+        serializer_lt = FlightListSerializer(flight_lt)
+
+        self.assertIn(serializer_eq.data, response.data["results"])
+        self.assertIn(serializer_qt.data, response.data["results"])
+        self.assertNotIn(serializer_lt.data, response.data["results"])
+
+    def test_filter_flights_by_date_arrival(self):
+        flight_eq = sample_flight(
+            departure_time="2024-08-27 15:00:00+03:00",
+            arrival_time="2024-08-27 17:00:00+03:00",
+        )
+        flight_qt = sample_flight(
+            departure_time="2024-08-28 15:00:00+03:00",
+            arrival_time="2024-08-28 17:00:00+03:00",
+        )
+        flight_lt = sample_flight(
+            departure_time="2024-08-26 15:00:00+03:00",
+            arrival_time="2024-08-26 17:00:00+03:00",
+        )
+
+        response = self.client.get(
+            FLIGHT_URL, {"date_arrival": "2024-08-27"}
+        )
+
+        serializer_eq = FlightListSerializer(flight_eq)
+        serializer_qt = FlightListSerializer(flight_qt)
+        serializer_lt = FlightListSerializer(flight_lt)
+
+        self.assertIn(serializer_eq.data, response.data["results"])
+        self.assertIn(serializer_qt.data, response.data["results"])
+        self.assertNotIn(serializer_lt.data, response.data["results"])
